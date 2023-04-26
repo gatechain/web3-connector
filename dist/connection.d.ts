614ddf6cdee4a3891e0a3d56c8bf55e8f9dbbb8b
Thone Liu <thorne_lew@126.com>
2023-04-26 19:36:16 +0800

    feat(web3-react): 8.2.0

--- dist/connection.d.ts
+++ dist/connection.d.ts
@@ -1,9 +1,9 @@
-import { Web3ReactHooks } from '@web3-react/core';
-import { Connector } from '@web3-react/types';
-import { MetaMask } from './metamask';
-import { CoinbaseWallet } from '@web3-react/coinbase-wallet';
-import { WalletConnect } from '@web3-react/walletconnect';
-import { Connection, ConnectionType } from './types';
+import { Web3ReactHooks } from "@web3-react/core";
+import { Connector } from "@web3-react/types";
+import { MetaMask } from "./metamask";
+import { CoinbaseWallet } from "@web3-react/coinbase-wallet";
+import { WalletConnect } from "@web3-react/walletconnect";
+import { Connection, ConnectionType } from "./types";
 declare type URLMap = {
     [chainId: number]: string | string[];
 };
@@ -13,11 +13,11 @@ export declare function connect(connector: Connector): Promise<void>;
 export declare function getIsInjected(): boolean;
 export declare function getIsMetaMask(): boolean;
 export declare function getIsCoinbaseWallet(): boolean;
-export declare function getConnection(c: Connector | ConnectionType): Connection | {
+export declare function getConnection(c: Connector | ConnectionType): {
     connector: CoinbaseWallet;
     hooks: Web3ReactHooks;
     type: ConnectionType;
-};
+} | Connection;
 export declare function getConnectionName(connectionType: ConnectionType, isMetaMask?: boolean): "MetaMask" | "Injected" | "Coinbase Wallet" | "WalletConnect";
 export declare function useEagerlyConnect(onError?: Function): void;
 export declare function connectWallet(connectionType: ConnectionType): void;
